05de4d2d1e52f849f21807e3304bf7a33dc9a1d8
Akmal1202 <rahimxojayevakmal@gmail.com>
2024-03-26 19:47:22 +0500

    some changes

---
+++ source/repos/API/API/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using API.Model;
+
+
+class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+        var endpoint = new Uri("https://randomuser.me/api/?inc=gender,name,nat");
+        var result = await client.GetAsync(endpoint);
+
+        if (result.IsSuccessStatusCode)
+        {
+            var json = await result.Content.ReadAsStringAsync();
+            var userData = JsonSerializer.Deserialize<User>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            //
+        }
+    }
+}
